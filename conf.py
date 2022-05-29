7d05b3d1863fca9963eca1f5fb3de58170bca864
rockstarcubalife <104049632+rockstarcubalife@users.noreply.github.com>
2022-05-29 12:49:27 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
-api_hash = os.environ.get("api_hash")
-api_id = os.environ.get('api_id')
-bot_token = os.environ.get("bot_token")
-authorized_users = ['']
-admin_user = os.environ.get('administrador')
+api_hash = "0c780f6db3124fafbdc3dcde437e6f21"
+api_id = 16536294
+bot_token = "5345114558:AAELuVzPHq-vDmpWUc9TlQ1R1dakfBhj10w"
+authorized_users = ['Eliel_010']
+admin_user = 'rockstar984'
